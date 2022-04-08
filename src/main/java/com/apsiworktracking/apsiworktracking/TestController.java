a5e87d533b2bbc95815d4479876ca06a9f50673f
ilyashich <ilyashich99@gmail.com>
2022-04-08 20:24:19 +0200

    added second test

--- src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
+++ src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
@@ -1,13 +1,22 @@
 package com.apsiworktracking.apsiworktracking;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class TestController
+{
 
     @GetMapping("/test")
-    public String test() {
+    public String test()
+    {
         return "Hello Github Action";
+    }
+
+    @GetMapping("/test2/{first}/{second}")
+    public String test2(@PathVariable int first, @PathVariable int second)
+    {
+        return first + " + " + second + " = " + (first + second);
     }
 }
