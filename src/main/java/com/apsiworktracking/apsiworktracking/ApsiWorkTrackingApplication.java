c62b429098963934c39d39e5bd3b9885df9c8d74
ilyashich <ilyashich99@gmail.com>
2022-04-08 19:21:01 +0200

    initial commit

---
+++ src/main/java/com/apsiworktracking/apsiworktracking/ApsiWorkTrackingApplication.java
@@ -0,0 +1,15 @@
+package com.apsiworktracking.apsiworktracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApsiWorkTrackingApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ApsiWorkTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apsiworktracking/apsiworktracking/ApsiWorkTrackingApplicationTests.java
@@ -0,0 +1,15 @@
+package com.apsiworktracking.apsiworktracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApsiWorkTrackingApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
