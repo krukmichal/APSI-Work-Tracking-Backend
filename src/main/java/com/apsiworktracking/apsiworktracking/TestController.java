67f6eb36e9fb68e47704af8267c949ba28c6f072
ilyashich <ilyashich99@gmail.com>
2022-04-08 22:53:34 +0200

    allow cross origin

--- src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
+++ src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
@@ -1,9 +1,11 @@
 package com.apsiworktracking.apsiworktracking;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class TestController
 {
