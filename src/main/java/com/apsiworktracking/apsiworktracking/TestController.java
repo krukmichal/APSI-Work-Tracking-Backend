62c1ebca2caf91b3d45ff7b478f569403c276b00
ilyashich <ilyashich99@gmail.com>
2022-04-08 23:30:42 +0200

    delete cross origin

--- src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
+++ src/main/java/com/apsiworktracking/apsiworktracking/TestController.java
@@ -1,6 +1,5 @@
 package com.apsiworktracking.apsiworktracking;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
