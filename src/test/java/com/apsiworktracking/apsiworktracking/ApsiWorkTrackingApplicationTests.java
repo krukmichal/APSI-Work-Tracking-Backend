d2831151207721b76e1b6e5cd781554cb8530ea7
ilyashich <ilyashich99@gmail.com>
2022-04-12 19:22:55 +0200

    works with local Postgres

--- src/test/java/com/apsiworktracking/apsiworktracking/ApsiWorkTrackingApplicationTests.java
+++ src/test/java/com/apsiworktracking/apsiworktracking/ApsiWorkTrackingApplicationTests.java
@@ -4,28 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootTest
 class ApsiWorkTrackingApplicationTests
 {
-    @Test
-    void test()
-    {
-        List<User> users = new ArrayList<>(Arrays.asList(
-                new User( "John", "123" ),
-                new User( "Mary", "456" ),
-                new User( "Peter", "789" )
-        ));
-
-        users.add(new User("Dave", "158"));
-
-        Assertions.assertEquals(4, users.size());
-        Assertions.assertEquals("John", users.get(0).getName());
-    }
-
     @Test
     void test2()
     {
