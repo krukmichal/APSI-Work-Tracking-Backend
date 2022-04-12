8ba1bb741f03ac0aad6b124ae63c9e1733ded6d9
ilyashich <ilyashich99@gmail.com>
2022-04-12 22:31:41 +0200

    added test function

--- src/main/java/com/example/apsitest/TestController.java
+++ src/main/java/com/example/apsitest/TestController.java
@@ -1,10 +1,14 @@
 package com.example.apsitest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController
 {
-    //hello world
-
+    @GetMapping("/test")
+    public String getTest()
+    {
+        return "Test";
+    }
 }
